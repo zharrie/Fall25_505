a294c851d02110c3b076d472d1bd29089c9852eb
unknown <you@example.com>
2025-09-09 18:33:04 -0400

    Add initial implementations for various lab exercises

---
+++ villalobosy1/labs/calories.py
@@ -0,0 +1,9 @@
+''' Calories = ((Age x 0.2757) + (Weight x 0.03295) + (Heart Rate x 1.0781) — 75.4991) x Time / 8.368 '''
+
+age = int(input())
+weight = int(input())
+heart_rate = int(input())
+time = int(input())
+
+calories = (((age * 0.2757) + (weight * 0.03295) + (heart_rate * 1.0781) -75.4991) * time ) / 8.368
+print(f"Calories: {calories:.2f} calories")
