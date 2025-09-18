02632d9368b1b696939524527c7feebd0501a51f
Daniel Pinto <danpin@Daniels-MacBook-Air.local>
2025-09-18 18:17:40 -0400

    Class Exercises 9/18/25

---
+++ pintod6/labs/class_session/exercises.py
@@ -0,0 +1,22 @@
+###############################################################
+# Practice Problems
+###############################################################
+# 1. Write a for loop that prints all the even numbers from 2 to 20.
+# 2. Write a while loop that calculates the sum of all integers from 1 to 100.
+# 3. Write a for loop that iterates over a string and prints each character on a new line.
+
+# for i in range(2, 21, 2):
+#     print(i)
+
+# i = 0
+# sum = 0
+# while i < 100:
+#     i +=1
+#     print(i)
+#     sum += i
+# print(sum)
+
+# fruits = "Kiwi"
+# for fruit in fruits:
+#     print(fruit)
+
