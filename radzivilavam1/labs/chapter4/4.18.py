0d2b968edee0cf73f2dc64721460ea92771bd89c
MargaritaRadz <radzivilavam1@montclair.edu>
2025-09-21 20:21:32 -0400

    Submitting chapter 4 labs and renaming folders

---
+++ radzivilavam1/labs/chapter4/4.18.py
@@ -0,0 +1,16 @@
+is_leap_year = False
+
+input_year = int(input())
+
+
+if (input_year % 100 == 0) and (input_year % 400 == 0):
+    is_leap_year = True
+elif (input_year % 4 == 0) and not(input_year % 100 == 0):
+    is_leap_year = True
+
+if is_leap_year:
+    print(f"{input_year} - leap year")
+else: 
+    print(f"{input_year} - not a leap year")
+
+
