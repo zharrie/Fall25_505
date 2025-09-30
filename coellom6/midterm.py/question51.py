00aeb4a841bf1c20fad79747dea4a81f8ca142cf
Maria Coello <mariacoello@Marias-MacBook-Pro.local>
2025-09-30 19:16:51 -0400

    publish coding assigment

---
+++ coellom6/midterm.py/question51.py
@@ -0,0 +1,20 @@
+# The program must define and call the following two functions. Define a function named max_number()
+# that takes four integer parameters and returns an integer representing the maximum of the four 
+#integers. Define a function named min_number() that takes four integer parameters and returns an 
+#integer representing the minimum of the four integers.
+
+
+#assign number to each variable or introduce number to code 
+num1 = 12
+num2 = 18 
+num3 = 4
+num4 = 9 
+# introduce integer input 
+max_number = int(input(num2)) 
+min_number = int(input(num3))
+
+def max_number(num1, num2, num3, num4)
+def min_number(num1, num2, num3, num4)
+#print statement to locate the numbers necessary 
+print("Maxium is", (max_number))
+print("Minimum is", (min_number))
---
+++ coellom6/midterm.py/question52.py
@@ -0,0 +1,25 @@
+#When analyzing data sets, such as data for human heights or for human weights, a common step is to
+# adjust the data. This can be done by normalizing to values between 0 and 1, or throwing away 
+#outliers. For this program, adjust the values by subtracting each value from the maximum. Input 
+#values should be added to the list until -1 is entered.
+#Your program must define and call the function:
+
+def get_max_int(nums)
+get_max_int = maximum value 
+get_max_int = int(input([0][1])) 
+#form a loop that allows postitive nums to be entered but make a stop once -1 integer is entered
+if get_max_int += "positive" 
+    print(nums)
+# allow for positive numbers to run and maje the break introducing int -1 
+elif: 
+    get_max_int == int(input(-1))
+    break 
+# if -1 integer is not introduce the get_max_int will return max values in the list 
+else: 
+    print(get_max_int()) 
+    
+print(get_max_int)  
+
+
+#Note: get_max_int() returns the maximum value in the list.
+
