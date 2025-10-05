dd58f5b900f9e4025410e935aa78ac527d6d7ea0
Maria Coello <mariacoello@Marias-MacBook-Pro.local>
2025-10-05 10:21:16 -0400

    cht9_10_11

---
+++ coellom6/labs/ch1_labs/ch2_labs.py
@@ -0,0 +1,12 @@
+# # Lab: Divide Input Integers 
+user_num=int(input())
+div_num=int(input())
+
+user_num = user_num // div_num
+print(user_num, end=' ')
+
+user_num = user_num // div_num
+print(user_num, end=' ')
+
+user_num = user_num // div_num
+print(user_num)
