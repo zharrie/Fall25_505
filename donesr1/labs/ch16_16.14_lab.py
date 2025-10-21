b007f3a7e339b38dfb6dd3e49399ebbaaef1fc50
Renee Dones <donesr1@montclair.edu>
2025-10-21 15:25:53 -0400

    ch 16 labs

---
+++ donesr1/labs/ch16_16.14_lab.py
@@ -0,0 +1,44 @@
+# TODO: Declare global variables here.
+recursions = 0
+comparisons = 0
+
+
+def binary_search(nums, target, lower, upper):
+    global recursions, comparisons
+    recursions += 1
+
+    if lower > upper:
+        return -1
+
+    mid = (upper + lower) //2
+
+    comparisons += 1
+    if nums[mid] == target:
+        return mid
+
+
+    if lower != upper:
+        comparisons += 1
+
+    if nums[mid] == target:
+        return mid
+    elif nums[mid] < target:
+        return binary_search(nums, target, mid + 1, upper)
+    else:
+        return binary_search(nums, target, lower, mid -1)
+
+
+
+if __name__ == "__main__":
+    # Input a list of nums from the first line of input
+    nums = [int(n) for n in input().split()]
+    
+    # Input a target value
+    target = int(input())
+
+    # Start off with default values: full range of list indices
+    index = binary_search(nums, target, 0, len(nums) - 1)
+
+    # Output the index where target was found in nums, and the
+    # number of recursions and comparisons performed
+    print(f"index: {index}, recursions: {recursions}, comparisons: {comparisons}")
